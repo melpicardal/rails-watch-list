e6862917e90d29c5a2ddfae92c75742d2f6b5a7c
meljun picardal <meljun.one@gmail.com>
2021-08-12 13:18:32 +0900

    working on routing, controller, views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,23 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new()
+  end
+
+  def create
+    @list = List.create(list_params)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists, only: %i[index show new create] do
+    resources :movies, only: %i[show] do
+      resources :bookmarks, only: %i[new create destroy]
+    end
+  end
 end
