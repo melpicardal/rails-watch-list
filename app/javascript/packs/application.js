fbab24263bbf0e3f2029d2509ce30f1cbb893027
meljun picardal <meljun.one@gmail.com>
2021-08-13 12:05:55 +0900

    deleted typed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
 import { initSelect2 } from '../components/init_select2';
-import { typed } from '../components/typed';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
